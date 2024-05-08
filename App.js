db5d171099f89746a098a4ceade8b50d3d4edca9
Lucivando Sousa <lucivandosousa@gmail.com>
2024-05-08 19:36:46 -0300

    feat: adicionar projeto

--- App.js
+++ App.js
@@ -1,20 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, Text, View } from 'react-native';
+
+import { styles } from './styles';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.message}>Hello World - 704</Text>
+      <Image
+        style={styles.image}
+        source={require("./assets/motorcycle.png")}
+      />
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ styles.js
@@ -0,0 +1,21 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#1E4B79',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  message: {
+    color: '#84D72F',
+    fontSize: 30,
+    borderWidth: 1,
+    borderColor: '#84D72F',
+    padding: 20,
+    borderRadius: 10
+  },
+  image: {
+    marginTop: 20
+  }
+});
